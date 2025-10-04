3efc0d680d2fbe2d5910bf0e09502b85d2de4ca2
mg 3 <135365423+iMG777@users.noreply.github.com>
2025-10-04 19:31:48 -0300

    Add files via upload

--- EstudAI/db.js
+++ EstudAI/db.js
@@ -1,8 +1,20 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,       // ⚠️ External DB URL para localhost
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,     // obrigatório para Render
+  },
 });
+
+// teste de conexão
+pool.query("SELECT NOW()")
+  .then(res => console.log("Conectado ao Postgres! Hora atual:", res.rows[0].now))
+  .catch(err => console.error("Erro ao conectar ao Postgres:", err));
 
 export default pool;
